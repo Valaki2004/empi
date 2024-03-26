6ebf18f30186af3eecd6ef0b04c94bd78d83f710
vtg <varastothgergo@ktch.hu>
2024-03-26 09:06:08 +0100

    haladás

---
+++ src/main.js
@@ -0,0 +1,15 @@
+var emps = 
+[
+    {
+        name:"Erős István",
+        city:"Szeged"
+    },
+    {
+        name:"Pala Irén",
+        city:"Hatvan"
+    }
+]
+
+console.log(emps[0].name)
+
+
