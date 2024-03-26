da8c59fc1d6e6def8523bd18845d96f11c6fe836
VTG <varastothgergo@ktch.hu>
2024-03-26 10:15:17 +0100

    müködik

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 var emps = 
 [
     {
+        id:1 ,
         name:"Erős István",
         city:"Szeged"
     },
     {
+        id:2 ,
         name:"Pala Irén",
         city:"Hatvan"
     }
@@ -12,4 +14,16 @@ var emps =
 
 console.log(emps[0].name)
 
-
+const tbody = document.querySelector("#tbody");
+var rows = ``;
+emps.forEach((emp) => {
+    var row = `
+    <tr>
+        <td>${emp.id}</td>
+        <td>${emp.name}</td>
+        <td>${emp.city}</td>
+    </tr>
+    `;
+    rows = rows + row;
+});
+tbody.innerHTML = rows;
